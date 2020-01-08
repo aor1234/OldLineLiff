6efc4f49fa9987fde24e98f061e17a909c30529f
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-08 17:45:19 +0700

    Add files via upload

---
+++ liff-starter.js
@@ -0,0 +1,41 @@
+window.onload = function (e) {
+    liff.init(function (data) {
+        initializeApp(data);
+    });
+};
+
+function initializeApp(data) {
+
+    // openWindow call
+
+
+    // closeWindow call
+
+    // sendMessages call
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
+        
+        var msg = document.getElementById('bchcode').value;
+        
+        liff.sendMessages([{
+
+            type: 'text',
+            text: msg
+            
+          
+        }, {
+            type: 'sticker',
+            packageId: '2',
+            stickerId: '144'
+        }]).then(function () {
+              window.alert("Message sent");
+        }).catch(function (error) {
+                window.alert("Error sending message: " + error);
+        });
+    });
+
+    // get access token
+   
+
+    // get profile call
+   
+}
