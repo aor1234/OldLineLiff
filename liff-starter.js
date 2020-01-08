dad716ca4df8c251ad8f63b404b9f08ec8e03b1f
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-08 17:59:50 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -5,37 +5,19 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-
-    // openWindow call
-
-
-    // closeWindow call
-
-    // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        
         var msg = document.getElementById('bchcode').value;
-        
         liff.sendMessages([{
-
             type: 'text',
             text: msg
-            
-          
         }, {
             type: 'sticker',
             packageId: '2',
             stickerId: '144'
         }]).then(function () {
-              window.alert("Message sent");
+            liff.closeWindow();
         }).catch(function (error) {
-                window.alert("Error sending message: " + error);
+            window.alert("Error sending message: " + error);
         });
     });
-
-    // get access token
-   
-
-    // get profile call
-   
 }
